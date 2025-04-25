1578425e15549b2ae00b49b4b23bf8058144b52c
Theadek <theadek10@gmail.com>
2025-04-25 20:15:50 +0200

    Add Terraform & aws version
    Seperate output to outputs.tf file

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+	required_version = ">= 1.0.0, < 2.0.0"
+
+	required_providers {
+	  aws = {
+		source = "hashicorp/aws"
+		version = "~> 4.0"
+	  }
+	}
+}
+
 provider "aws" {
 	region = "us-east-2"
 }
@@ -30,9 +41,4 @@ resource "aws_instance" "example"{
 	tags = {
 		Name = "First Terraform"
 	}
-}
-
-output "public_ip" {
-	value = aws_instance.example.public_ip
-	description = "The public IP address of the web server"
 }
